8feea2a02707a82596da6d80d61917674e4331d4
Your Name <you@example.com>
2018-07-03 02:35:45 +0800

    add department to option

--- js/index.js
+++ js/index.js
@@ -9,9 +9,11 @@ const vm = new Vue({
   el:'#app',
   data:{
     faculties:['全部','文學院','理學院','社科院','工學院','管學院','法學院','教育學院','其他'],
+    departments:[],
     codes:[],
     courses:[],
-    state:'0',
+    faculty:'0',
+    department:'0',
     filter:'',
   },
   mounted:function() {
@@ -25,29 +27,50 @@ const vm = new Vue({
     });
   },
   methods:{
-    change_state(id){
-      console.log(id);
-      this.state = id;
-      let $opt = $('.opt');
-      console.log($opt);
+    change_faculty_state(id){
 
-      Array.from($opt).forEach((i)=>{
-        console.log(i);
-        $(i).removeClass('highlight');
-      });
+      this.department='0';
+      this.faculty = id;
+
+      let $opt = $('.opt');
+
+      this.remove_class($opt);
+
       $opt = $($opt.get(id));
       $opt.addClass('highlight');
-    }
+    },
+    change_department_state(id){
+      this.department = this.codes[id];
+      // console.log(this.department);
+      let $opt = $('.departments .opt');
+      // let $opt = $(`.opt:contains(${id})`);
+      console.log($opt);
+      this.remove_class($opt);
+
+      $opt = $(`.opt:contains(${id})`);
+      $opt.addClass('highlight');
+    },
+    remove_class(target){
+      Array.from(target).forEach((i)=>{
+        // console.log(i);
+        $(i).removeClass('highlight');
+      });
+    },
   },
   computed:{
     filter_courses:function () {
       let results=[];
+      this.departments=[];
       for(department in this.courses){
-        if((!(department.match(new RegExp('^[a-z]','i'))&&this.state=='8'))&&(this.state!=department[0])&&this.state!='0') continue;
+        if((!(department.match(new RegExp('^[a-z]','i'))&&this.faculty=='8'))&&(this.faculty!=department[0])&&this.faculty!='0') continue;
+        if(this.faculty!='0'){
+          this.departments.push(this.codes[department]);
+        }
+        if(department!=this.department&&this.department!='0') continue;
         this.courses[department].forEach((course)=>{
           const flag = course.class_name.toLowerCase().indexOf(this.filter.toLowerCase());
           if(flag!=-1){
-            course['faculty'] = this.codes[department];
+            course['department'] = this.codes[department];
             results.push(course);
           }
         });
