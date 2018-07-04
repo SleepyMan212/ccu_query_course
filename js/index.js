a7fe9c9331942fe000fe8794df8a6307e6d4f381
Your Name <you@example.com>
2018-07-04 09:55:42 +0800

    add the query_option about 老師名稱

--- js/index.js
+++ js/index.js
@@ -35,6 +35,9 @@ const vm = new Vue({
     faculty:'0',
     department:'0',
     filter:'',
+    query_options:['課程名稱','老師名稱'],
+    query_transalte:{'課程名稱':'class_name','老師名稱':'teacher'},
+    selected:'課程名稱',
   },
   mounted:function() {
     $.getJSON("code_table.json").then((res)=>{
@@ -45,6 +48,7 @@ const vm = new Vue({
       this.courses = res
       console.log("courses read sucessly");
     });
+    console.log(this.query_transalte['課程名稱']);
   },
   methods:{
     change_faculty_state(id){
@@ -80,25 +84,23 @@ const vm = new Vue({
         $(i).removeClass('highlight');
       });
     },
-    open_outline(){
-      // console.log(course);
-      console.log("AA");
-      // window.open(course.outline, 'Yahoo', config='height=500,width=500');
-    },
 
   },
   computed:{
     filter_courses:function () {
       let results=[];
       this.departments=[];
+      const query_item = this.query_transalte[this.selected];
+      // console.log(query_item);
       for(department in this.courses){
         if((!(department.match(new RegExp('^[a-z]','i'))&&this.faculty=='8'))&&(this.faculty!=department[0])&&this.faculty!='0') continue;
         if(this.faculty!='0'){
           this.departments.push(this.codes[department]);
         }
         if(department!=this.department&&this.department!='0') continue;
+
         this.courses[department].forEach((course)=>{
-          const flag = course.class_name.toLowerCase().indexOf(this.filter.toLowerCase());
+          const flag = course[query_item].toLowerCase().indexOf(this.filter.toLowerCase());
           if(flag!=-1){
             course['department'] = this.codes[department];
             results.push(course);
